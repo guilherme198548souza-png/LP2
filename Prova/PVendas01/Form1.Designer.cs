24df61537027acb6168e809c2e12a724ec5ccb70
guilherme souza <guilherme198548.souza@gmail.com>
2025-10-31 20:32:04 -0300

    Add files via upload

---
+++ Prova/PVendas01/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PVendas01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lstbxTotal = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(257, 47);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(124, 88);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(257, 171);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(124, 101);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lstbxTotal
+            // 
+            this.lstbxTotal.FormattingEnabled = true;
+            this.lstbxTotal.ItemHeight = 20;
+            this.lstbxTotal.Location = new System.Drawing.Point(579, 47);
+            this.lstbxTotal.Name = "lstbxTotal";
+            this.lstbxTotal.Size = new System.Drawing.Size(216, 344);
+            this.lstbxTotal.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 460);
+            this.Controls.Add(this.lstbxTotal);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ListBox lstbxTotal;
+    }
+}
+
---
+++ Prova/PVendas01/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PVendas01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double[,] data = new double[3, 4];
+            String auxiliar = "";
+            String saida = "";
+            double soma = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    auxiliar = Interaction.InputBox($"Digite o valor {j + 1} do Mês {i + 1}", "Entrada de Dados");
+                    if (data[i, j] < 0)
+                    {
+                        MessageBox.Show("Dado Inválido");
+                    }
+                    else
+                    {
+                        soma = data[i,j] + soma;
+
+                    }
+                }
+                saida += $"";
+
+                Convert.ToString(soma);
+       
+                lstbxTotal.Items.Add($"Total do Mês {i + 1}: semana:{soma}");
+            }
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lstbxTotal.ClearSelected(); 
+        }
+    }
+}
